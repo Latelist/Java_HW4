3cf8f404e0cf82a5b66ef962cf7085996bcbba3e
Latelist <94052381+Latelist@users.noreply.github.com>
2023-06-23 02:39:39 +0600

    Update Task3.java
    deleted unused imports

--- Task3.java
+++ Task3.java
@@ -3,9 +3,6 @@
 
 import java.io.FileWriter;
 import java.io.IOException;
-// import java.nio.file.Files;
-// import java.nio.file.Paths;
-
 import java.util.Date;
 import java.text.SimpleDateFormat;
 import java.util.Scanner;
